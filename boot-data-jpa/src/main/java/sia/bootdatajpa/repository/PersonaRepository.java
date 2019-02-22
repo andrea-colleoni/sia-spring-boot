c6b15ef446a52a7aadd300b70fae3738f1d2cca4
Andrea Colleoni <andrea@colleoni.info>
2019-02-22 14:00:05 +0100

    config repository

--- boot-data-jpa/src/main/java/sia/bootdatajpa/repository/PersonaRepository.java
+++ boot-data-jpa/src/main/java/sia/bootdatajpa/repository/PersonaRepository.java
@@ -1,8 +1,13 @@
 package sia.bootdatajpa.repository;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.Future;
+import java.util.stream.Stream;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Repository;
 
 import sia.bootdatajpa.model.Persona;
@@ -10,6 +15,19 @@ import sia.bootdatajpa.model.Persona;
 @Repository
 public interface PersonaRepository extends PagingAndSortingRepository<Persona, Integer> {
 	
-	List<Persona> findByCognome(String cognome);
+	@Async
+	Future<List<Persona>> findByCognome(String cognome);
+	
+	List<Persona> findByAltezzaInCmBetween(int min, int max);
+	
+	@Query("select max(p.id) from Persona p")
+	Long getMaxId();
+	
+	@Query(value= "select * from persona", nativeQuery=true)
+	List<Persona> queryNativa();
+	
+	@Query(value = "select * from persona where numero_scarpe = ?1 limit 1", nativeQuery=true)
+	Optional<Persona> primaPersonaConScarpe(int numero);
 
+	Stream<Persona> findByPesoInKgGreaterThan(float pesoMinimo);
 }
--- boot-data-jpa/src/test/java/sia/bootdatajpa/repository/PersonaRepositoryTest.java
+++ boot-data-jpa/src/test/java/sia/bootdatajpa/repository/PersonaRepositoryTest.java
@@ -1,9 +1,15 @@
 package sia.bootdatajpa.repository;
 
-import static org.junit.Assert.fail;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 
 import java.util.Date;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,12 +18,16 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import sia.bootdatajpa.model.Persona;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class PersonaRepositoryTest {
+	
+	static Logger log = LogManager.getLogger(PersonaRepositoryTest.class);
 	
 	@Autowired
 	private PersonaRepository personaRepository;
@@ -31,7 +41,7 @@ public class PersonaRepositoryTest {
 				);
 		
 		for(Persona p : persone) {
-			System.out.println(p);
+			log.info(p);
 		}
 	}
 
@@ -42,7 +52,7 @@ public class PersonaRepositoryTest {
 		Iterable<Persona> persone = personaRepository.findAll(pageable);
 		
 		for(Persona p : persone) {
-			System.out.println(p);
+			log.info(p);
 		}
 	}
 
@@ -59,12 +69,64 @@ public class PersonaRepositoryTest {
 		personaRepository.save(p);
 	}
 	
+	@Test(expected = Exception.class)
+	public void testById() throws Exception {
+		Persona p = personaRepository
+					.findById(7)
+					.orElseThrow(() -> new Exception());
+	}
+	
 	@Test
-	public void testFindBy() {
-		Iterable<Persona> persone = personaRepository.findByCognome("Rossi");
+	public void testScarpe() throws Exception {
+		Persona p = personaRepository.primaPersonaConScarpe(43)
+				.orElseThrow(() -> new Exception());
+		assertNotNull(p);
+		p = personaRepository
+				.primaPersonaConScarpe(50)
+				.orElse(null);
+		assertNull(p);
+	}
+	
+	@Test
+	public void testFindBy() throws InterruptedException, ExecutionException {
+		List<Persona> persone = personaRepository.findByCognome("Rossi").get();
 		for(Persona p : persone) {
-			System.out.println(p);
+			log.info(p);
 		}
+		
+		persone = personaRepository.findByAltezzaInCmBetween(179, 182);
+
+		for(Persona p : persone) {
+			log.info(p);
+		}
+		
+		log.info(personaRepository.getMaxId());
+		
+		persone = personaRepository.queryNativa();
+		for(Persona p : persone) {
+			log.info(p);
+		}
+	}
+	
+	@Test
+	@Transactional(propagation=Propagation.REQUIRES_NEW)
+	public void testFindAllPesanti() {
+		personaRepository.findByPesoInKgGreaterThan(50.0F)
+			.parallel()
+			.forEach(p -> {
+//				p.setAltezzaInCm(p.getAltezzaInCm() + 1);
+//				personaRepository.save(p);
+				log.info(p);
+			});
+	}
+	
+	@Test
+	public void testAsync() throws InterruptedException, ExecutionException {
+		Future<List<Persona>> persone = personaRepository.findByCognome("Rossi");
+		for(Persona p : persone.get()) {
+			log.info(p);
+		}
+		log.info("finito.");
 	}
 
 }
