6be25ac8de6b36d9fcf4359573b5d092ca29316c
Andrea Colleoni <andrea@colleoni.info>
2019-02-27 10:09:10 +0100

    sistemazione springfox

--- boot-data-jpa/src/main/java/sia/bootdatajpa/BootDataJpaApplication.java
+++ boot-data-jpa/src/main/java/sia/bootdatajpa/BootDataJpaApplication.java
@@ -1,33 +1,31 @@
 package sia.bootdatajpa;
 
+import static springfox.documentation.schema.AlternateTypeRules.newRule;
+
 import java.time.LocalDate;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.context.request.async.DeferredResult;
 
 import com.fasterxml.classmate.TypeResolver;
 
 import sia.bootdatajpa.model.Persona;
-import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.builders.ResponseMessageBuilder;
-import springfox.documentation.schema.ModelRef;
 import springfox.documentation.schema.WildcardType;
 import springfox.documentation.service.Tag;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.data.rest.configuration.SpringDataRestConfiguration;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-import static com.google.common.collect.Lists.*;
-import static springfox.documentation.schema.AlternateTypeRules.*;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
 @SpringBootApplication
-@EnableSwagger2
+@EnableSwagger2WebMvc
+@Import(SpringDataRestConfiguration.class)
 public class BootDataJpaApplication {
 	
 	@Autowired
@@ -51,23 +49,23 @@ public class BootDataJpaApplication {
 		                typeResolver.resolve(ResponseEntity.class, WildcardType.class)),
 		                typeResolver.resolve(WildcardType.class)))
 		        .useDefaultResponseMessages(false)
-		        .globalResponseMessage(RequestMethod.GET,
-		            newArrayList(new ResponseMessageBuilder()
-		                .code(500)
-		                .message("500 message")
-		                .responseModel(new ModelRef("Error"))
-		                .build()))
+//		        .globalResponseMessage(RequestMethod.GET,
+//		            newArrayList(new ResponseMessageBuilder()
+//		                .code(500)
+//		                .message("500 message")
+//		                .responseModel(new ModelRef("Error"))
+//		                .build()))
 		        //.securitySchemes(newArrayList(apiKey()))
 		        //.securityContexts(newArrayList(securityContext()))
 		        .enableUrlTemplating(true)
-		        .globalOperationParameters(
-		            newArrayList(new ParameterBuilder()
-		                .name("someGlobalParameter")
-		                .description("Description of someGlobalParameter")
-		                .modelRef(new ModelRef("string"))
-		                .parameterType("query")
-		                .required(true)
-		                .build()))
+//		        .globalOperationParameters(
+//		            newArrayList(new ParameterBuilder()
+//		                .name("someGlobalParameter")
+//		                .description("Description of someGlobalParameter")
+//		                .modelRef(new ModelRef("string"))
+//		                .parameterType("query")
+//		                .required(true)
+//		                .build()))
 		        .tags(new Tag("Persone Service", "All apis relating to persons")) 
 		        .additionalModels(typeResolver.resolve(Persona.class)) 
 		        ;
